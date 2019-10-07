29ca2ebd587d9fbbd63bbac0394ff00df66455d7
momoepita <37119553+momoepita@users.noreply.github.com>
2019-10-07 20:53:16 +0200

    Add files via upload

--- manipulation et traitement d'image/chargement d'image/binarize.c
+++ manipulation et traitement d'image/chargement d'image/binarize.c
@@ -4,15 +4,58 @@
 #include <stdlib.h>
 #include "binarize.h"
 
+int main()
+{
+    SDL_Surface *ecran = NULL, *imageDeFond = NULL;
+    SDL_Rect positionFond;
+
+    positionFond.x = 0;
+    positionFond.y = 0;
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    /* Chargement de l'icône AVANT SDL_SetVideoMode en 16*16 */
+    SDL_WM_SetIcon(SDL_LoadBMP("sdl_icone.bmp"), NULL);
+    //use IMG_Load to load any image format
+    //use SDL_LoadBMP for a bmp
+
+    ecran = SDL_SetVideoMode(500, 400, 32, SDL_HWSURFACE);
+    SDL_WM_SetCaption("Chargement d'images en SDL", NULL);
+
+    /* Chargement d'une image Bitmap dans une surface */
+    imageDeFond = SDL_LoadBMP("image4.bmp");
+    /* On blitte par-dessus l'écran */
+    SDL_BlitSurface(imageDeFond, NULL, ecran, &positionFond);
+
+    grayscale(ecran,500,400);
+
+    SDL_Flip(ecran);
+
+    pause();
+
+    SDL_FreeSurface(imageDeFond); /* On libère la surface */
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
+}
+
 void grayscale(SDL_Surface *surface, size_t height,size_t width){
-    for (int i = 0; i < height; ++i) {
-        for (int j = 0; j < width; ++j) {
+    for (size_t i = 0; i < height; ++i) {
+        for (size_t j = 0; j < width; ++j) {
             Uint8 R=0;
             Uint8 G=0;
             Uint8 B=0;
-            SDL_GetRGB(getpixel(surface,i,j),ecran->format,&R,&G,&B);
+            SDL_GetRGB(getpixel(surface,i,j),surface->format,&R,&G,&B);
             //n'arrondi pas à l'entier supérieur(à modifier dans le pire des cas)
             Uint8 gray=0.2125 * R + 0.7154 * G + 0.0721 * B;
+
+            //je laisse ça ici pour une nuance de noire et de blanc
+            /*if(gray>255/2){
+                gray=255;
+            }
+            else{
+                gray=0;
+            }*/
             setPixel(surface,i,j,gray,gray,gray);
         }
 
--- manipulation et traitement d'image/chargement d'image/binarize.h
+++ manipulation et traitement d'image/chargement d'image/binarize.h
@@ -1,6 +1,6 @@
 #ifndef BINARIZE_H
 #define BINARIZE_H
-
+void grayscale(SDL_Surface *surface, size_t height,size_t width);
 void pause();
 Uint32 getpixel(SDL_Surface *surface, int x, int y);
 void setPixel(SDL_Surface* dst, int x, int y, size_t r,size_t g,size_t b);
